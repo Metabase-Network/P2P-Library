dc3e3cb200f92110b5d22fb17e8c3d8e1b51ce1b
18dew <harsh.patel54@gmail.com>
2018-09-27 18:56:16 +0530

    Add Peer

--- protobuf/protobuf.go
+++ protobuf/protobuf.go
@@ -14,4 +14,10 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package peer
+package protobuf
+
+type NodeID struct {
+	PublicKey []byte
+	Address   string
+	ID        []byte
+}
