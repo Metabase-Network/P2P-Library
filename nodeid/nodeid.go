a970c362261d9d56d164330e1248551174840441
18dew <harsh.patel54@gmail.com>
2018-09-28 13:19:43 +0530

    Update

--- nodeid/nodeid.go
+++ nodeid/nodeid.go
@@ -14,12 +14,4 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-package nodeid
-
-import (
-	"fmt"
-)
-
-func test() {
-	fmt.Println("This is test")
-}
+package dht
