e1526b1e1ef0efe7f29fac9e13505a55e0c1be7e
18dew <harsh.patel54@gmail.com>
2018-10-10 14:50:09 +0530

    Update Node level key store

--- go.mod
+++ go.mod
@@ -1,7 +1,9 @@
 module github.com/Metabase-Network/vasuki
 
 require (
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/pkg/errors v0.8.0
+	github.com/syndtr/goleveldb v0.0.0-20180815032940-ae2bd5eed72d
 	golang.org/x/crypto v0.0.0-20180718160520-a2144134853f
 	golang.org/x/sys v0.0.0-20180925112736-b09afc3d579e // indirect
 )
---
+++ nodep/nodep.go
@@ -0,0 +1,89 @@
+// Copyright 2014 The Metabase Authors
+// This file is part of vasuki p2p library.
+//
+// The vasuki library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The vasuki library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package nodep
+
+import (
+	"encoding/hex"
+
+	"github.com/Metabase-Network/vasuki/crypto"
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+type nodePks struct {
+	StorPath string
+	Stordb   *leveldb.DB
+}
+
+func start(path string) nodePks {
+	var ret nodePks
+	db, err := leveldb.OpenFile(path, nil)
+	defer db.Close()
+	if err == nil {
+		ret = nodePks{StorPath: path, Stordb: db}
+	} else {
+		ret = nodePks{}
+	}
+	return ret
+}
+
+func (n nodePks) fetchKey() string {
+	var ret string
+	if n.Stordb != nil {
+		res, err := n.Stordb.Get([]byte("vasukiNodeKey"), nil)
+		if err != nil {
+			ret = hex.EncodeToString(res)
+		} else {
+			ret = ""
+		}
+	}
+	return ret
+}
+
+func (n nodePks) iskeySet() bool {
+	var ret1 bool
+	if n.Stordb != nil {
+		res, err := n.Stordb.Get([]byte("vasukiNodeKey"), nil)
+		if err != nil {
+			ret := hex.EncodeToString(res)
+			_, err1 := crypto.HexToECDSA(ret)
+			if err1 != nil {
+				ret1 = true
+			} else {
+				ret1 = false
+			}
+		} else {
+			ret1 = false
+		}
+	}
+	return ret1
+}
+
+func (n nodePks) setkey() bool {
+	var ret bool
+	if n.Stordb != nil {
+		key, _ := crypto.GenerateKey()
+		err := n.Stordb.Put([]byte("vasukiNodeKey"), crypto.FromECDSA(key), nil)
+		if err != nil {
+			ret = true
+		} else {
+			ret = false
+		}
+	} else {
+		ret = false
+	}
+	return ret
+}
