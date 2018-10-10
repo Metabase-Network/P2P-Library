57c2bcd3b1f0a74e73229f97f0afe0582447785e
18dew <harsh.patel54@gmail.com>
2018-10-10 18:49:38 +0530

    Update stor/stor_test.go

--- stor/stor_test.go
+++ stor/stor_test.go
@@ -26,7 +26,7 @@ var testPrivHex = "289c2857d4598e37fb9647507e47a309d6133539bf21a8b9cb6df88fd5232
 
 func TestPath(t *testing.T) {
 	path := "./testing"
-	err, n := start(path)
+	err, n := Start(path)
 
 	fmt.Println(err, n)
 }
